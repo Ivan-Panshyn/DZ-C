a4dd396dc0ce2ee4435509b8a20a34f522dd3637
Panshyn Ivan <vanya.pantera2517@gmail.com>
2023-03-12 17:22:09 +0300

    Добавлен второй метод в Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,8 +4,20 @@
 // 782 -> 8
 // 918 -> 1
 
+
+// Первый метод
 Console.WriteLine("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int secondDigit = number / 10 % 10;
 Console.WriteLine($"Вторая цифра -> {secondDigit}");
+
+// Второй метод
+int delFirstLastDigit = DelFirstLastDigit(number);
+Console.WriteLine($"Полученное однозначное число -> {delFirstLastDigit}");
+
+int DelFirstLastDigit(int num) 
+{
+  int result = number / 10 % 10;
+  return result;
+}
