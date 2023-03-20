1a7d4c9a112a47b579680901e3222a57b98bd1cc
Panshyn Ivan <vanya.pantera2517@gmail.com>
2023-03-20 23:21:00 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, 
+// 1. который принимает на вход два числа (A и B) и 
+// 2. возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int naturnNumber = NaturnNumber(numberA, numberB);
+Console.WriteLine("Степень числа: " + naturnNumber);
+
+int NaturnNumber(int numberA, int numberB)
+{
+  int result = 1;
+  for (int i = 1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+  return result;
+}
