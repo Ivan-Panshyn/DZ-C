1d381824a34e2866b6d0ddbce862a82abfb56181
Panshyn Ivan <vanya.pantera2517@gmail.com>
2023-03-25 15:20:32 +0300

    Added Task34

---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, 
+// 1. которая задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+FillArray(array);
+WriteArray(array);
+
+void FillArray(int[] arr)
+{
+  Random rand = new Random();
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    arr[i] = rand.Next(0, 99);
+  }
+}
+
+void WriteArray(int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + "  ");
+    Console.Write($"{arr[arr.Length - 1]}");
+  }
+  Console.Write($"{arr[arr.Length - 1]}");
+  Console.WriteLine("]");
+}
