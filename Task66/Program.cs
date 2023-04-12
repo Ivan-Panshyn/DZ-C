7f5e17d91f81e4e317f6539723d73d085d45e25c
Panshyn Ivan <vanya.pantera2517@gmail.com>
2023-04-12 20:35:33 +0300

    Added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// 1. Задайте значения M и N. 
+// 2. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// 3. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите целое число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM > numberN)
+{
+  int sumNumber = SumNumber(numberM, numberN);
+  Console.WriteLine(sumNumber);
+}
+else if (numberM < numberN)
+{
+  int sumNumber = SumNumber(numberN, numberM);
+  Console.WriteLine(sumNumber);
+}
+else Console.WriteLine("Введены одинаковые значение М и N");
+
+
+int SumNumber(int num1, int num2)
+{
+  if (num1 < num2) return 0;
+  else return num1 + SumNumber(num1 - 1, num2);
+}
